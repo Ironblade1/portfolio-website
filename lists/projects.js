1de9827a1a1e0896cfb2ab0f3c88c5a96e8c8748
Rohit Kumar <83993262+Ironblade1@users.noreply.github.com>
2023-12-19 20:20:46 +0530

    Update projects.js

--- lists/projects.js
+++ lists/projects.js
@@ -14,6 +14,11 @@ export const projects = {
     "Desc": "a website where you will analysis your whatsapp messages",
     "Tags": ["Python", "Matplotlib", "Pandas", "Streamlit"]
   },
+  "Appointment-Schedular": {
+    "Link": "https://github.com/Ironblade1/Appointment-schedular",
+    "Desc": "Built a React web app facilitating streamlined appointment scheduling and visualization for trainers and clients.",
+    "Tags": ["ReactJs", "JavaSccript", "Bootstrap", "SyncFusion"]
+  }
   "ATM-Machine": {
     "Link": "https://github.com/Ironblade1/ATM-Machine",
     "Desc": "an automated Teller machine with the user's Account Number, and Password",
